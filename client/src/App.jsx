119b7a3ace9c3518b17cca9656e04ff08f9363f2
Garvit Goyal <garvitgoyal2k6@gmail.com>
2026-05-24 19:56:39 +0530

    fixing bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Login from "./pages/Login"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Dashboard from "./pages/Dashboard"
 import Register from "./pages/Register"
 import {useEffect} from "react"
@@ -13,6 +13,7 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/register" element={<Register />} />
